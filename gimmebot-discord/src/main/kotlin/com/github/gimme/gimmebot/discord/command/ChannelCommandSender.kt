3de80cd4f0c6463cb30361044006256f480f6c1d
Anders Magnusson <anders14magnusson@gmail.com>
2020-12-19 18:37:13 +0100

    docs: Add missing documentation

--- gimmebot-discord/src/main/kotlin/com/github/gimme/gimmebot/discord/command/ChannelCommandSender.kt
+++ gimmebot-discord/src/main/kotlin/com/github/gimme/gimmebot/discord/command/ChannelCommandSender.kt
@@ -4,7 +4,13 @@ import com.github.gimme.gimmebot.core.command.CommandSender
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.User
 
+/**
+ * Represents a command sender in Discord channels that the bot is part of.
+ *
+ * This includes both text channels in servers and private messages.
+ */
 class ChannelCommandSender(private val channel: MessageChannel, private val user: User) : CommandSender {
+
     override val name: String
         get() = "[DISCORD] ${user.name}"
 
--- gimmebot-discord/src/main/kotlin/com/github/gimme/gimmebot/discord/config/DiscordConfig.kt
+++ gimmebot-discord/src/main/kotlin/com/github/gimme/gimmebot/discord/config/DiscordConfig.kt
@@ -1,6 +1,10 @@
 package com.github.gimme.gimmebot.discord.config
 
+/**
+ * Discord bot configuration.
+ */
 data class DiscordConfig(
+
     /** Discord bot authentication token. */
     var token: String = "",
     /** Command prefix. */
