467d443c30f1d932240070c1abb90612b6fa1d5b
Anders Magnusson <anders14magnusson@gmail.com>
2020-12-20 11:58:34 +0100

    test: Add an extra assertion for safety

--- gimmebot-core/src/test/kotlin/com/github/gimme/gimmebot/core/command/BaseCommandTest.kt
+++ gimmebot-core/src/test/kotlin/com/github/gimme/gimmebot/core/command/BaseCommandTest.kt
@@ -3,6 +3,7 @@ package com.github.gimme.gimmebot.core.command
 import com.github.gimme.gimmebot.core.command.executor.CommandExecutor
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -56,6 +57,8 @@ class BaseCommandTest {
                 called = true
             }
         }
+
+        assertFalse(called)
 
         command.execute(DUMMY_COMMAND_SENDER,
             listOf("string", "", "1", "-999", "0.5", "36", "trUE", "false", "aaa"))
