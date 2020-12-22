a90cb3b3abeb719603a4405172454cdfbf8161a9
Anders Magnusson <anders14magnusson@gmail.com>
2020-12-23 00:59:14 +0100

    feat: Add generic type to command error responses

--- gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/ErrorResponses.kt
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/ErrorResponses.kt
@@ -2,16 +2,16 @@ package com.github.gimme.gimmebot.core.command
 
 
 /** An argument has the wrong format. */
-val INVALID_ARGUMENT_ERROR = error("Invalid argument")
+fun <T> INVALID_ARGUMENT_ERROR() = error<T>("Invalid argument")
 
 /** The command does not accept the type of the current command sender. */
-val INCOMPATIBLE_SENDER_ERROR = error("You cannot use that command")
+fun <T> INCOMPATIBLE_SENDER_ERROR() = error<T>("You cannot use that command")
 
 /** Too few arguments supplied with the command. */
-val TOO_FEW_ARGUMENTS_ERROR = error("Too few arguments")
+fun <T> TOO_FEW_ARGUMENTS_ERROR() = error<T>("Too few arguments")
 
 /** Too many arguments supplied with the command. */
-val TOO_MANY_ARGUMENTS_ERROR = error("Too many arguments")
+fun <T> TOO_MANY_ARGUMENTS_ERROR() = error<T>("Too many arguments")
 
 
-private fun error(message: String) = CommandResponse(message, CommandResponse.Status.ERROR)
+private fun <T> error(message: String) = CommandResponse<T>(message, CommandResponse.Status.ERROR)
