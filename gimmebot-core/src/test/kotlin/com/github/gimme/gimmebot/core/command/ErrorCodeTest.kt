7d776162d7d0b69ab787b36f7b1f8088ddc32580
Anders Magnusson <anders14magnusson@gmail.com>
2020-12-23 18:19:20 +0100

    test: Add ErrorCode tests

---
+++ gimmebot-core/src/test/kotlin/com/github/gimme/gimmebot/core/command/ErrorCodeTest.kt
@@ -0,0 +1,20 @@
+package com.github.gimme.gimmebot.core.command
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ErrorCodeTest {
+
+    private val errorCode = ErrorCode.INVALID_ARGUMENT
+
+    @Test
+    fun `command exception should include message`() {
+        assertEquals(errorCode.message, errorCode.createException().message)
+    }
+
+    @Test
+    fun `command exception with context should show context after colon`() {
+        val context = "context"
+        assertEquals(errorCode.message + ": $context", errorCode.createException(context).message)
+    }
+}
