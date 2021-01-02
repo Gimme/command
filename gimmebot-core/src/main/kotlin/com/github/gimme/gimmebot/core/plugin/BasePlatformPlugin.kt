9f4f7173e3fcd5606f7b84d218aad79280f953f7
Anders Magnusson <anders14magnusson@gmail.com>
2021-01-02 14:18:49 +0100

    feat: Add Enableable interface

--- gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/plugin/BasePlatformPlugin.kt
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/plugin/BasePlatformPlugin.kt
@@ -2,11 +2,12 @@ package com.github.gimme.gimmebot.core.plugin
 
 import com.github.gimme.gimmebot.core.GimmeBot
 import com.github.gimme.gimmebot.core.command.manager.CommandManager
+import com.github.gimme.gimmebot.core.common.Enableable
 
 /**
  * Represents a plugin that can be installed in a [GimmeBot] to support a new platform.
  */
-abstract class BasePlatformPlugin {
+abstract class BasePlatformPlugin : Enableable {
     /** The bot that this plugin is installed in. */
     protected lateinit var bot: GimmeBot
         private set
@@ -14,22 +15,14 @@ abstract class BasePlatformPlugin {
     /** The plugin-specific command manager */
     abstract val commandManager: CommandManager<*>
 
-    /** If this plugin is enabled. */
-    var enabled: Boolean = false
+    override var enabled: Boolean = false
         set(enabled) {
-            if (field == enabled) return
-            field = enabled
-
-            if (enabled) onEnable() else onDisable()
+            field = Enableable.enable(this, enabled)
         }
+
+    override fun onDisable() {}
 
     internal fun init(bot: GimmeBot) {
         this.bot = bot
     }
-
-    /** Performs logic when enabled. */
-    protected abstract fun onEnable()
-
-    /** Performs logic when disabled. */
-    protected open fun onDisable() {}
 }
