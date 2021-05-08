d97c8364db2e0a2ba1d38a9275d3a15e4f4af4a3
Anders Magnusson <anders14magnusson@gmail.com>
2021-05-08 12:51:37 +0200

    refactor: Extract CommandParameterType to its own file

---
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/CommandParameterType.kt
@@ -0,0 +1,28 @@
+package com.github.gimme.gimmebot.core.command
+
+import com.github.gimme.gimmebot.core.command.exception.CommandException
+import com.github.gimme.gimmebot.core.command.exception.ErrorCode
+
+/**
+ * Represents a command parameter type used to manage input argument values.
+ *
+ * @property name the display name of this parameter type.
+ */
+abstract class CommandParameterType<T>(val name: String) where T : Any {
+
+    /**
+     * Converts the [input] to this parameter type.
+     *
+     * @throws CommandException if the [input] is invalid
+     */
+    @Throws(CommandException::class)
+    fun convert(input: Any): T = convertOrNull(input) ?: throw ErrorCode.INVALID_ARGUMENT.createException(input)
+
+    /**
+     * Converts the [input] to this parameter type, or null if failed to convert.
+     *
+     * @throws CommandException if the internal conversion failed
+     */
+    @Throws(CommandException::class)
+    abstract fun convertOrNull(input: Any): T?
+}
