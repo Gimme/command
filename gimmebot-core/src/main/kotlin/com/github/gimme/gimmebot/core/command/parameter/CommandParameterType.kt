689da8e7dab542b64e41df74312f0297b745e6cb
Anders Magnusson <anders14magnusson@gmail.com>
2021-05-30 19:43:05 +0200

    feat: Allow adding a custom error message for command parameter types

--- gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/CommandParameterType.kt
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/CommandParameterType.kt
@@ -6,13 +6,15 @@ import com.github.gimme.gimmebot.core.command.exception.ErrorCode
 /**
  * Represents a command parameter type used to manage input argument values.
  *
- * @property name the display name of this parameter type
- * @property values gets all possible values this parameter type can have, or null if undefined
+ * @property name          the display name of this parameter type
+ * @property values        gets all possible values this parameter type can have, or null if undefined
+ * @property errorMessage  message to be included if the input is invalid and cannot be converted to this type
  * @property convertOrNull converts the input to this parameter type, or null if failed to convert
  */
 class CommandParameterType<T>(
     val name: String,
     val values: (() -> Set<String>)? = null,
+    val errorMessage: String? = "Not a \"$name\"",
     var convertOrNull: (Any) -> T?
 ) where T : Any {
 
@@ -22,5 +24,7 @@ class CommandParameterType<T>(
      * @throws CommandException if the [input] is invalid
      */
     @Throws(CommandException::class)
-    fun convert(input: Any): T = convertOrNull(input) ?: throw ErrorCode.INVALID_ARGUMENT.createException(input)
+    fun convert(input: Any): T =
+        convertOrNull(input)
+            ?: throw ErrorCode.INVALID_ARGUMENT.createException("$input" + (errorMessage?.let { " ($it)" } ?: ""))
 }
