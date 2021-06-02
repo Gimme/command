ed5751ef01ef21bcc8a7028e16962a157139bd4e
Anders Magnusson <anders14magnusson@gmail.com>
2021-06-02 18:24:32 +0200

    feat: Make CommandParameterSet mutable

--- gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/CommandParameterSet.kt
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/CommandParameterSet.kt
@@ -5,14 +5,13 @@ package com.github.gimme.gimmebot.core.command.parameter
  *
  * The main purpose of this class is to supply efficient lookup methods.
  */
-class CommandParameterSet(parameters: Collection<CommandParameter> = listOf()): Set<CommandParameter> {
+class CommandParameterSet(parameters: Collection<CommandParameter> = listOf()): MutableSet<CommandParameter> {
 
-    private val parameters: List<CommandParameter> = parameters.toList()
-    private val parameterById: Map<String, CommandParameter> = parameters.map { it.id to it }.toMap()
-    private val parameterByFlag: Map<Char, CommandParameter>
+    private val parameters: MutableSet<CommandParameter> = parameters.toMutableSet()
+    private val parameterById: MutableMap<String, CommandParameter> = parameters.map { it.id to it }.toMap().toMutableMap()
+    private val parameterByFlag: MutableMap<Char, CommandParameter> = mutableMapOf()
 
     init {
-        parameterByFlag = mutableMapOf()
         parameters.reversed().forEach { param ->
             param.flags.forEach { flag -> parameterByFlag[flag] = param }
         }
@@ -45,4 +44,52 @@ class CommandParameterSet(parameters: Collection<CommandParameter> = listOf()): 
     override fun containsAll(elements: Collection<CommandParameter>): Boolean = elements.all { containsId(it.id) }
 
     override fun isEmpty(): Boolean = parameters.isEmpty()
+
+    override fun add(element: CommandParameter): Boolean {
+        if (!parameters.add(element)) return false
+        parameterById[element.id] = element
+        element.flags.forEach { flag -> parameterByFlag[flag] = element }
+
+        return true
+    }
+
+    override fun addAll(elements: Collection<CommandParameter>): Boolean {
+        if (!parameters.addAll(elements)) return false
+        parameterById.putAll(elements.map { it.id to it }.toMap() as MutableMap<String, CommandParameter>)
+        elements.reversed().forEach { param ->
+            param.flags.forEach { flag -> parameterByFlag[flag] = param }
+        }
+
+        return true
+    }
+
+    override fun clear() {
+        parameters.clear()
+        parameterById.clear()
+        parameterByFlag.clear()
+    }
+
+    override fun remove(element: CommandParameter): Boolean {
+        if (!parameters.remove(element)) return false
+        parameterById.remove(element.id)
+        parameterByFlag.values.remove(element)
+
+        return true
+    }
+
+    override fun removeAll(elements: Collection<CommandParameter>): Boolean {
+        if (!parameters.removeAll(elements)) return false
+        parameterById.values.removeAll(elements)
+        parameterByFlag.values.removeAll(elements)
+
+        return true
+    }
+
+    override fun retainAll(elements: Collection<CommandParameter>): Boolean {
+        if (!parameters.retainAll(elements)) return false
+        parameterById.values.retainAll(elements)
+        parameterByFlag.values.retainAll(elements)
+
+        return true
+    }
 }
