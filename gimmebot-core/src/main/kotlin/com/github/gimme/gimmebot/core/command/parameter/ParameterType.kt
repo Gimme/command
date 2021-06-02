8cdc7a871e0542927d419b524cb3ea71d0635471
Anders Magnusson <anders14magnusson@gmail.com>
2021-06-02 18:25:41 +0200

    style: Rename type parameter

--- gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/ParameterType.kt
+++ gimmebot-core/src/main/kotlin/com/github/gimme/gimmebot/core/command/parameter/ParameterType.kt
@@ -5,13 +5,13 @@ import com.github.gimme.gimmebot.core.command.exception.CommandException
 /**
  * Represents a command parameter type used to handle input argument values.
  *
- * @param R                the real type that this represents
+ * @param T                the real type that this represents
  * @property name          the display name of this parameter type
  * @property values        gets all possible values this parameter type can have, or null if undefined
  * @property singular      if this type represents singular or plural values
  * @property errorMessage  message to be included if an input value is invalid and cannot be converted to this type
  */
-interface ParameterType<R> {
+interface ParameterType<T> {
 
     val name: String
     val values: (() -> Set<String>)?
@@ -24,5 +24,5 @@ interface ParameterType<R> {
      * @throws CommandException if the [input] is invalid
      */
     @Throws(CommandException::class)
-    fun convert(input: Collection<String>): R
+    fun convert(input: Collection<String>): T
 }
