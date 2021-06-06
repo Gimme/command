b12ae35abc8e475af007ce23f1a24feed8ca95de
Anders Magnusson <anders14magnusson@gmail.com>
2021-06-06 22:29:44 +0200

    feat: Add functional interfaces

--- gimmebot-boot/src/main/kotlin/com/github/gimme/gimmebot/boot/command/property/DelegateProperty.kt
+++ gimmebot-boot/src/main/kotlin/com/github/gimme/gimmebot/boot/command/property/DelegateProperty.kt
@@ -2,16 +2,16 @@ package com.github.gimme.gimmebot.boot.command.property
 
 import kotlin.reflect.KProperty
 
-interface Property<out T, in S> {
+fun interface Property<out T, in S> {
 
     operator fun provideDelegate(thisRef: S, property: KProperty<*>): Delegate<T, S>
 }
 
-interface Delegate<out T, in S> {
+fun interface Delegate<out T, in S> {
 
     operator fun getValue(thisRef: S, property: KProperty<*>): T
 }
 
 
-interface CommandProperty<out T> : Property<T, PropertyCommand<*>>
-interface CommandDelegate<out T> : Delegate<T, PropertyCommand<*>>
+fun interface CommandProperty<out T> : Property<T, PropertyCommand<*>>
+fun interface CommandDelegate<out T> : Delegate<T, PropertyCommand<*>>
